3e88632b63bca1ded4e595d998c527305a0af8a8
Shelley Ophir <qidcoder@gmail.com>
2025-07-16 22:59:34 -0400

    conditional navigation

--- app/src/main/java/com/example/androidnavigation/GameFragment.kt
+++ app/src/main/java/com/example/androidnavigation/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.androidnavigation.R
 import com.example.androidnavigation.databinding.FragmentGameBinding
 
@@ -98,9 +99,11 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
+                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
+                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
                 }
             }
         }
