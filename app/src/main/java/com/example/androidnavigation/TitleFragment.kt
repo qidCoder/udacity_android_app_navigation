859ae2ff46d27e7d5ef5b97b875cebc5b88bb71b
Shelley Ophir <qidcoder@gmail.com>
2025-07-17 07:28:56 -0400

    create overflow menu on title fragment

--- app/src/main/java/com/example/androidnavigation/TitleFragment.kt
+++ app/src/main/java/com/example/androidnavigation/TitleFragment.kt
@@ -3,12 +3,16 @@ package com.example.androidnavigation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.Navigator
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.androidnavigation.databinding.FragmentTitleBinding
 
 //
@@ -53,7 +57,21 @@ class TitleFragment : Fragment() {
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
 
+        // tell Android we will have a menu associated with our Title fragment
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    // when a menu item is selected, this function will be called
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 //    companion object {
