62940c9b681f9e91575387c75cbdb35cfdee51e2
Shelley Ophir <qidcoder@gmail.com>
2025-07-18 10:40:07 -0400

    implement left side navigation drawer

--- app/src/main/java/com/example/androidnavigation/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigation/MainActivity.kt
@@ -6,23 +6,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
 import com.example.androidnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        drawerLayout = binding.drawerLayout
+
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        // setup navigation UI to know about the navigation view
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return  navController.navigateUp()
+        // replace up button with drawer button in the start destination - title fragment
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
